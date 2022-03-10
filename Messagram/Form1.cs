0a60fe11d8401f39d384bbe835696746091ffdf9
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-03-10 11:29:48 -0800

    Initial commit

---
+++ Messagram/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Messagram
+{
+    public partial class Form1 : Form
+    {
+        public Thread server_thread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                server_thread = new Thread(Messagram.connect);
+                server_thread.Start();
+                Thread.Sleep(5000); // Wait 5 seconds to connect to Messagram Server
+                if (Messagram.connected)
+                {
+                    MessageBox.Show("Connected to Messagram Chat!", "Success");
+                }
+                else
+                {
+                    MessageBox.Show("Unable to connect to Messagram Chat, Try again later!", "Error");
+                }
+            } catch
+            {
+                MessageBox.Show("Error", "Unable to connect to Messagram Chat, Try again later!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(()=>Messagram.send_command("testing this\n"));
+            t.Start();
+            Thread.Sleep(1000);
+            t.Abort();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+
+    class Messagram
+    {
+        // Messagram Info & Status
+        public static string messagram_ip = "skrillec.ovh";
+        public static Int32 messagram_port = 30;
+        public static bool connected = false;
+
+        // System.Net.Sockets Objects
+        public static TcpClient client;
+        public static NetworkStream stream;
+
+        // Threads
+        public static Thread listener_thread;
+
+        // Connect To Messagram Server
+        public static void connect()
+        {
+            Messagram.client = new TcpClient(messagram_ip, messagram_port);
+            Messagram.connected = true;
+            listener_thread = new Thread(Messagram.listener);
+            listener_thread.Start();
+        }
+
+        public static void listener()
+        {
+            Byte[] data = new byte[256];
+            string Response = String.Empty;
+            NetworkStream strm = Messagram.client.GetStream();
+            Messagram.stream = strm;
+            while (true) {
+
+                Int32 b = strm.Read(data, 0, data.Length);
+                Response = System.Text.Encoding.ASCII.GetString(data, 0, b);
+            }
+
+        }
+
+        public static void send_command(string msg)
+        {
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(msg);
+            Messagram.stream.Write(data, 0, data.Length);
+        }
+
+    }
+}
---
+++ Messagram/Form2.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Messagram
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(658, 375);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 393);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(546, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 431);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
